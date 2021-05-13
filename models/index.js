11227279c4191ff3cdc51368f8303c7250d43261
Lacey <lacey.griffith@g.austincc.edu>
2021-05-13 14:28:39 -0500

    product update error with foreign key

--- models/index.js
+++ models/index.js
@@ -20,14 +20,12 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
-  //onDelete: 'CASCADE'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
-  // onDelete: 'CASCADE'
 })
 
 
