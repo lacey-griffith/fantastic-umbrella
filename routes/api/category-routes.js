fd301499ac7b4ade47474e92d9cf94354a3ec3ad
Lacey <lacey.griffith@g.austincc.edu>
2021-05-13 09:24:34 -0500

    delete error for product and category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,7 +64,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  //[x] delete a category by its `id` value
+  //[] delete a category by its `id` value
   Category.destroy({
     where: {
       id: req.params.id
